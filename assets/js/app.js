e0eb3204ef4112895cd3a350da060160ce36da3c
satan-69 <hopfner.charles@gmail.com>
2020-05-11 21:58:59 +0200

    separation des events shooting et divers

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,5 +22,3 @@ import '../../public/css/style.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
